89cfca4afbf6d2096a674660d39ffd1f80f481e8
bound1ess <bound1ess.dev@gmail.com>
2015-01-24 17:42:12 +0600

    Part of Message.

--- src/Adviser/Messages/Message.php
+++ src/Adviser/Messages/Message.php
@@ -1,5 +1,56 @@
 <?php namespace Adviser\Messages;
 
+use InvalidArgumentException;
+
 class Message {
-    // @todo
+
+    /**
+     * @var integer
+     */
+    const NORMAL = 2;
+
+    /**
+     * @var integer
+     */
+    const WARNING = 3;
+
+    /**
+     * @var integer
+     */
+    const ERROR = 4;
+
+    /**
+     * @var string
+     */
+    protected $message;
+
+    /**
+     * @var integer
+     */
+    protected $level;
+
+    /**
+     * @param string $message
+     * @param int $level
+     * @return Message
+     */
+    public function __construct($message, $level) {
+        $this->message = (string) $message;
+        $this->setLevel($level);
+    }
+
+    /**
+     * Validate and set appropriate level.
+     *
+     * @throws InvalidArgumentException
+     * @param integer $level
+     * @return void
+     */
+    protected function setLevel($level) {
+        if ( ! in_array($level, [static::NORMAL, static::WARNING, static::ERROR])) {
+            throw new InvalidArgumentException;
+        }
+
+        $this->level = $level;
+    }
 }
--- testing/tests/Adviser/Messages/MessageTest.php
+++ testing/tests/Adviser/Messages/MessageTest.php
@@ -1,5 +1,14 @@
 <?php namespace Adviser\Messages;
 
 class MessageTest extends \PHPUnit_Framework_TestCase {
-    // @todo
+
+    /** @test */ function it_sets_level() {
+        $message = new Message("some message", Message::NORMAL);
+    }
+
+    /** @test */ function it_throws_an_exception_if_invalid_level_is_passed() {
+        $this->setExpectedException("InvalidArgumentException");
+
+        new Message("some message", null);
+    }
 }
