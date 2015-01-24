9bc85109e45874a7ac20d7a538c1cb793ab6dd97
bound1ess <bound1ess.dev@gmail.com>
2015-01-24 17:24:59 +0600

    Implement and test MessageBag (100%).

--- src/Adviser/Messages/MessageBag.php
+++ src/Adviser/Messages/MessageBag.php
@@ -1,5 +1,36 @@
 <?php namespace Adviser\Messages;
 
 class MessageBag {
-    // @todo
+
+    /**
+     * @var array
+     */
+    protected $messages;
+
+    /**
+     * @param array $messages
+     * @return MessageBag
+     */
+    public function __construct(array $messages = []) {
+        $this->messages = $messages;
+    }
+
+    /**
+     * Add a new message to the bag.
+     *
+     * @param Message $message
+     * @return void
+     */
+    public function throwIn(Message $message) {
+        $this->messages[] = $message;
+    }
+
+    /**
+     * Return all messages stored in the bag.
+     *
+     * @return array
+     */
+    public function getAll() {
+        return $this->messages;
+    }
 }
--- testing/tests/Adviser/Messages/MessageBagTest.php
+++ testing/tests/Adviser/Messages/MessageBagTest.php
@@ -1,5 +1,27 @@
 <?php namespace Adviser\Messages;
 
+use Mockery;
+
 class MessageBagTest extends \PHPUnit_Framework_TestCase {
-    // @todo
+
+    protected $bag;
+
+    public function setUp() {
+        $this->bag = new MessageBag;
+    }
+
+    public function tearDown() {
+        Mockery::close();
+    }
+
+    protected function createMessage() {
+        return Mockery::mock("Adviser\Messages\Message");
+    }
+
+    /** @test */ function it_adds_a_message() {
+        $this->assertCount(0, $this->bag->getAll());
+        $this->bag->throwIn($this->createMessage());
+        $this->bag->throwIn($this->createMessage());
+        $this->assertCount(2, $this->bag->getAll());
+    }
 }
