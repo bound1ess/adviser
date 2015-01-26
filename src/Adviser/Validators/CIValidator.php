9b30b9304cf0ca2ef93b533ec8751b49a6b67946
bound1ess <bound1ess.dev@gmail.com>
2015-01-26 19:48:31 +0600

    Finish CIValidator.

--- src/Adviser/Validators/CIValidator.php
+++ src/Adviser/Validators/CIValidator.php
@@ -6,10 +6,63 @@ class CIValidator extends AbstractValidator
 {
 
     /**
+     * @var array
+     */
+    protected $allowedVersions = ["hhvm", "5.6", "5.5", "5.4"];
+
+    /**
      * @inheritdoc
      */
     public function handle()
     {
-        // @todo
+        $bag = new MessageBag();
+
+        $bag->throwIn($this->checkTravisConfigurationFile());
+
+        return $bag;
+    }
+
+    /**
+     * @return Message
+     */
+    protected function checkTravisConfigurationFile()
+    {
+        if ( ! $this->utility("File")->exists($path = $this->directory."/.travis.yml")) {
+            return new Message(
+                "Your project doesn't seem to be using Travis CI platform (travis-ci.org).",
+                Message::ERROR
+            );
+        }
+
+        $config = $this->utility("YAMLParser")->parse(file_get_contents($path));
+
+        if ( ! array_key_exists("php", $config) or ! $this->checkVersions($config["php"])) {
+            return new Message(
+                "Your .travis.yml file seems to be using the outdated versions of PHP.",
+                Message::WARNING
+            );
+        }
+
+        return new Message(
+            "Your project uses Travis and the settings are fine.",
+            Message::NORMAL
+        );
+    }
+
+    /**
+     * Look for outdated versions of PHP.
+     *
+     * @param array $versions
+     * @return boolean
+     */
+    protected function checkVersions(array $versions)
+    {
+        foreach ($versions as $version) {
+            if ( ! in_array($version, $this->allowedVersions)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- testing/tests/Adviser/Validators/CIValidatorTest.php
+++ testing/tests/Adviser/Validators/CIValidatorTest.php
@@ -3,6 +3,12 @@
 use Mockery;
 use Adviser\Messages\Message;
 
+// Mock for testing purposes.
+function file_get_contents()
+{
+    return null;
+}
+
 class CIValidatorTest extends ValidatorTestCase
 {
 
@@ -12,11 +18,41 @@ class CIValidatorTest extends ValidatorTestCase
         $validator = new CIValidator(null);
 
         $file = Mockery::mock("Adviser\Utility\File");
+
+        $file->shouldReceive("exists")
+             ->times(3)
+             ->with("/.travis.yml")
+             ->andReturn(false, true, true);
+
         $YAMLParser = Mockery::mock("Adviser\Utility\YAMLParser");
+
+        $YAMLParser->shouldReceive("parse")
+                   ->twice()
+                   ->andReturn(
+                       ["php" => ["5.1", "5.2", "5.3", "weird"]],
+                       ["php" => ["5.4", "5.5", "5.6", "hhvm"]]
+                   );
 
         $validator->utility("File", $file);
         $validator->utility("YAMLParser", $YAMLParser);
 
         // Test.
+        // 1st scenario: no .travis.yml file was found in the root directory of the project.
+        $messages = $validator->handle();
+        $this->isMessageBag($messages);
+
+        $this->assertEquals($messages->first()->getLevel(), Message::ERROR);
+
+        // 2nd scenario: .travis.yml file is present, but PHP versions are outdated.
+        $messages = $validator->handle();
+        $this->isMessageBag($messages);
+
+        $this->assertEquals($messages->first()->getLevel(), Message::WARNING);
+
+        // 3rd scenario: everything is fine.
+        $messages = $validator->handle();
+        $this->isMessageBag($messages);
+
+        $this->assertEquals($messages->first()->getLevel(), Message::NORMAL);
     }
 }
