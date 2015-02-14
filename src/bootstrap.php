f5cf903ed59a7c5af9b7a867b3a5e9bf82334a5d
bound1ess <bound1ess.dev@gmail.com>
2015-02-14 19:29:31 +0600

    1.0.7: minor fixing.

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,28 +5,32 @@ if ( ! defined("ADVISER_DIR")) {
     define("ADVISER_DIR", __DIR__."/..");
 }
 
-/**
- * Attempt to find the autoloader.
- *
- * @return mixed
- */
-function findComposerAutoloader()
-{
-    if (file_exists($path = ADVISER_DIR."/vendor/autoload.php")) {
-        return require_once $path;
-    }
+if ( ! function_exists("findComposerAutoloader")) {
+    /**
+     * Attempt to find the autoloader.
+     *
+     * @return mixed
+     */
+    function findComposerAutoloader()
+    {
+        if (file_exists($path = ADVISER_DIR."/vendor/autoload.php")) {
+            return require_once $path;
+        }
 
-    return require_once ADVISER_DIR."/../../autoload.php";
+        return require_once ADVISER_DIR."/../../autoload.php";
+    }
 }
 
-/**
- * Get Adviser's current version.
- *
- * @return string
- */
-function getAdviserVersion()
-{
-    $configuration = (new Adviser\ConfigurationLoader())->load();
+if ( ! function_exists("getAdviserVersion")) {
+    /**
+     * Get Adviser's current version.
+     *
+     * @return string
+     */
+    function getAdviserVersion()
+    {
+        $configuration = (new Adviser\ConfigurationLoader())->load();
 
-    return $configuration["version"];
+        return $configuration["version"];
+    }
 }
